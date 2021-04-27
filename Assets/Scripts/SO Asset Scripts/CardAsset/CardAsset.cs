cb3f1baca4c335bab659d5db3f86042aa9aa975f
sagigvili <57597612+sagigvili@users.noreply.github.com>
2021-04-27 17:32:20 +0300

    Initial Arena + Cards

---
+++ Assets/Scripts/SO Asset Scripts/CardAsset/CardAsset.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public enum MonsterAttribute
+{
+    Fire,
+    Water,
+    Wind,
+    Earth,
+    Light,
+    Dark
+}
+
+public enum MonsterType
+{
+    Dragon,
+    Fiend,
+    Spellcaster,
+    Fairy,
+    Machine
+}
+public class CardAsset : ScriptableObject
+{
+    // this object will hold the info about the most general card
+    [Header("General Info")]
+    public CharacterAsset characterAsset;  // if this is null, it`s a neutral card
+    [TextArea(2,3)]
+    public string Description;  // Description for spell or character
+	public Sprite CardImage;
+
+    [Header("Monster Info")]
+    public int Attack;
+    public int Defence;
+    public MonsterAttribute Attribue;
+    public MonsterType Type;
+    public int rank;
+
+}
---
+++ Assets/Scripts/Visual/CardVisualScripts/OneCardManager.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+// holds the refs to all the Text, Images on the card
+public class OneCardManager : MonoBehaviour {
+
+    public CardAsset cardAsset;
+    public OneCardManager PreviewManager;
+    [Header("Text Component References")]
+    public Text NameText;
+    public Text TypeText;
+    public Text DescriptionText;
+    [Header("Image References")]
+    public Image CardImageFront;
+    public Image CardImageBack;
+
+    void Awake()
+    {
+        if (cardAsset != null)
+            ReadCardFromAsset();
+    }
+
+    private bool canBePlayedNow = false;
+    public bool CanBePlayedNow
+    {
+        get
+        {
+            return canBePlayedNow;
+        }
+
+        set
+        {
+            canBePlayedNow = value;
+        }
+    }
+
+    public void ReadCardFromAsset()
+    {
+        // 2) add card name
+        NameText.text = cardAsset.name;
+        // 4) add description
+        DescriptionText.text = cardAsset.Description;
+
+        if (PreviewManager != null)
+        {
+            // this is a card and not a preview
+            // Preview GameObject will have OneCardManager as well, but PreviewManager should be null there
+            PreviewManager.cardAsset = cardAsset;
+            PreviewManager.ReadCardFromAsset();
+        }
+    }
+}
